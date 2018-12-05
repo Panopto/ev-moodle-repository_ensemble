459b2499ec3b0502825d76ae0540497adc603175
Jim Pease <jmpease@codeserv.us>
2018-12-05 09:55:26 -0500

    Version bump

--- version.php
+++ version.php
@@ -28,8 +28,8 @@ defined('MOODLE_INTERNAL') || die();
 
 // You are on the MOODLE_35_STABLE branch.  Do NOT update the version
 // branching date rather update the release increment.
-$plugin->version        = 2018060601;
-$plugin->release        = '3.5 (Build: 2018060601)';
+$plugin->version        = 2018060602;
+$plugin->release        = '3.5 (Build: 2018060602)';
 $plugin->maturity       = MATURITY_STABLE;
 $plugin->requires       = 2018051700;
 $plugin->component      = 'repository_ensemble';
