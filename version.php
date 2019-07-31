1c33b11d6ae48897c8a16eb29629bd4e7029e61c
Jim Pease <jmpease@codeserv.us>
2019-07-31 13:28:45 -0400

    Updated version for Moodle 3.7 dev.

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-// You are on the MOODLE_36_STABLE branch.  Do NOT update the version
+// You are on the MOODLE_37_STABLE branch.  Do NOT update the version
 // branching date rather update the release increment.
-$plugin->version        = 2018121701;
-$plugin->release        = '3.6 (Build: 2018121701)';
+$plugin->version        = 2019073100;
+$plugin->release        = '3.7 (Build: 2019073100)';
 $plugin->maturity       = MATURITY_STABLE;
-$plugin->requires       = 2018120300;
+$plugin->requires       = 2019051100;
 $plugin->component      = 'repository_ensemble';
-$plugin->dependencies   = array('filter_ensemble' => 2018121700);
+$plugin->dependencies   = array('filter_ensemble' => 2019073100);
